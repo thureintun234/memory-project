8c7dd80f47d6e47997fe2eb7f9b6d97531e695cc
thureintun234 <thurein12456@gmail.com>
2021-10-10 11:52:24 +0630

    first

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import dotenv from 'dotenv'
+
+import postRouters from './routes/posts.js'
+import userRouters from './routes/users.js'
+dotenv.config()
+
+const app = express()
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }))
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
+app.use(cors())
+
+app.use('/posts', postRouters)
+app.use('/user', userRouters)
+
+const PORT = process.env.PORT || 5000
+
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
+    .catch(error => console.log(error.message))
+
+mongoose.set('useFindAndModify', false)
